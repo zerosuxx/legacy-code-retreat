278a025f928dd5a3209690683f8324cee3258c53
Mohos Tamás <tomi@mohos.name>
2018-09-11 14:38:11 +0200

    feat(Categories): add getLastCategory method

--- src/Categories.php
+++ src/Categories.php
@@ -5,18 +5,37 @@ namespace App;
 class Categories
 {
 
+    private $categories = [];
     private $categoriesByPlace = [];
+    private $questions = [];
+    private $lastCategoryName = null;
 
-    public function add(string $name, array $places, int $point): void
+    public function addCategory(Category $category): void
     {
-        foreach ($places as $place) {
-            $this->categoriesByPlace[$place] = [$name, $point];
+        $this->categories[$category->getName()] = $category;
+        foreach ($category->getPlaces() as $place) {
+            $this->categoriesByPlace[$place] = $category;
         }
     }
 
-    public function getCategoryByPlace(int $place)
+    public function getCategoryByPlace(int $place): Category
     {
         return $this->categoriesByPlace[$place];
     }
 
+    public function addQuestion(string $categoryName, Question $question)
+    {
+        $this->questions[$categoryName][] = $question;
+    }
+
+    public function getQuestionByCategoryName(string $categoryName): Question
+    {
+        $this->lastCategoryName = $categoryName;
+        return array_shift($this->questions[$categoryName]);
+    }
+
+    public function getLastCategory(): Category
+    {
+        return $this->categories[$this->lastCategoryName];
+    }
 }
--- test/Unit/CategoriesTest.php
+++ test/Unit/CategoriesTest.php
@@ -2,19 +2,53 @@
 
 namespace Test\Unit;
 
+use App\Categories;
+use App\Category;
+use App\Question;
 use PHPUnit\Framework\TestCase;
 
 class CategoriesTest extends TestCase
 {
 
     /**
- * @test
- */
+     * @test
+     */
     public function getCategoryByPlace_returnsCategory()
     {
-        $categories = new \App\Categories();
-        $categories->add('Pop', [1,2,3], 1);
+        $categories = new Categories();
+        $category = new Category('Pop', [1,2,3], 1);
+        $categories->addCategory($category);
 
-        $this->assertEquals(['Pop', 1], $categories->getCategoryByPlace(1));
+        $this->assertEquals($category, $categories->getCategoryByPlace(1));
+    }
+
+    /**
+     * @test
+     */
+    public function getQuestionByCategoryName_returnsQuestion()
+    {
+        $q1 = new Question('Pop question 1');
+        $q2 = new Question('Pop question 2');
+        $categories = new Categories();
+        $categories->addQuestion('Pop', $q1);
+        $categories->addQuestion('Pop', $q2);
+
+        $this->assertEquals($q1, $categories->getQuestionByCategoryName('Pop'));
+        $this->assertEquals($q2, $categories->getQuestionByCategoryName('Pop'));
+    }
+
+    /**
+     * @test
+     */
+    public function getLastCategory_returnsLastCategory()
+    {
+        $question = new Question('Pop question 1');
+        $categories = new Categories();
+        $category = new Category('Pop', [1,2,3], 1);
+        $categories->addCategory($category);
+        $categories->addQuestion('Pop', $question);
+        $categories->getQuestionByCategoryName('Pop');
+
+        $this->assertEquals($category, $categories->getLastCategory());
     }
 }
