6a576b2fdabcb76dd3cb8ed75c8f25ca1a0053d7
Mohos Tamás <tomi@mohos.name>
2018-09-11 14:48:15 +0200

    feat(GameRunner): add GameRunner class

--- GameRunner.php
+++ GameRunner.php
@@ -1,41 +1,30 @@
 <?php
 
-use App\Category;
+require __DIR__ . '/Game.php';
+require __DIR__ . '/vendor/autoload.php';
 
-include __DIR__.'/Game.php';
-include __DIR__.'/vendor/autoload.php';
+class GameRunner
+{
+    private $notAWinner;
 
-$notAWinner;
+    public function run()
+    {
+        $aGame = new Game(require 'game.config.php');
 
-$categories = new \App\Categories();
-$categories->addCategory(new Category('Pop', [0, 4, 8], 1));
-$categories->addCategory(new Category('Science', [1, 5, 9], 1));
-$categories->addCategory(new Category('Sports', [2, 6, 10], 1));
-$categories->addCategory(new Category('Rock', [3, 7, 11], 1));
-foreach(['Pop', 'Science', 'Sports', 'Rock'] as $category) {
-    foreach(range(0, 50) as $i) {
-        $categories->addQuestion($category, new \App\Question($category . ' Question ' . $i));
+        $aGame->add("Chet");
+        $aGame->add("Pat");
+        $aGame->add("Sue");
+
+        do {
+            $aGame->roll(rand(0, 5) + 1);
+
+            if (rand(0, 9) === 7) {
+                $this->notAWinner = $aGame->wrongAnswer();
+            } else {
+                $this->notAWinner = $aGame->wasCorrectlyAnswered();
+            }
+        } while ($this->notAWinner);
     }
 }
 
-$aGame = new Game($categories);
-
-$aGame->add("Chet");
-$aGame->add("Pat");
-$aGame->add("Sue");
-
-
-do {
-
-    $aGame->roll(rand(0,5) + 1);
-
-    if (rand(0,9) == 7) {
-        $notAWinner = $aGame->wrongAnswer();
-    } else {
-        $notAWinner = $aGame->wasCorrectlyAnswered();
-    }
-
-
-
-} while ($notAWinner);
-
+(new GameRunner())->run();
---
+++ game.config.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Categories;
+use App\Category;
+
+$categories = new Categories();
+$categories->addCategory(new Category('Pop', [0, 4, 8], 1));
+$categories->addCategory(new Category('Science', [1, 5, 9], 1));
+$categories->addCategory(new Category('Sports', [2, 6, 10], 1));
+$categories->addCategory(new Category('Rock', [3, 7, 11], 1));
+foreach(['Pop', 'Science', 'Sports', 'Rock'] as $category) {
+    foreach(range(0, 50) as $i) {
+        $categories->addQuestion($category, new \App\Question($category . ' Question ' . $i));
+    }
+}
+return $categories;
