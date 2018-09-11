3717fcab60b28e733b0ef2e597d9799f23731280
Mohos Tamás <tomi@mohos.name>
2018-09-11 11:08:19 +0200

    logs moved to test original & actual add Test class

--- test.php
+++ test.php
@@ -1,3 +1,38 @@
 <?php
 
-function compare($file1, $file2){}
+class TestCase
+{
+
+    public function run($tests = 10)
+    {
+        $success = true;
+        for ($i = 1; $i <= $tests; $i++) {
+            $success = $this->test($i);
+            echo '.';
+        }
+        if($success) {
+            echo "\n\033[1;32mOK ({$tests} tests)\033[0m\n";
+        } else {
+            echo "\n\033[1;41mERROR ({$tests} tests)\033[0m\n";
+        }
+    }
+
+    public function assertFileEquals($file1, $file2)
+    {
+        return assert(file_get_contents($file1) === file_get_contents($file2), 'Two file are not equals!');
+    }
+
+    public function test($seed, $captureFile = 'test_capture.php')
+    {
+        $originalPath = 'test/original/';
+        $actualPath = 'test/actual/';
+
+        $filename = sprintf('game_%d.txt', $seed);
+
+        shell_exec(sprintf('php %s %d test > %s', $captureFile, $seed, $actualPath . $filename));
+        return $this->assertFileEquals($originalPath . $filename, $actualPath . $filename);
+    }
+}
+
+$test = new TestCase();
+$test->run();
--- test_capture.php
+++ test_capture.php
@@ -1,16 +1,7 @@
 <?php
 
-
 $seed = isset($argv[1]) ? $argv[1] : 1;
-$version = isset($argv[2]) ? '_'.$argv[2] : '';
 
 mt_srand($seed);
 
-ob_start();
-
 include __DIR__ . '/GameRunner.php';
-
-$contents = ob_get_clean();
-
-file_put_contents('logs/game_' . $seed . $version . ' .txt', $contents);
-
