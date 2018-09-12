98840e89eacfd51a92717e4d18f9397416b55766
Mohos Tamás <tomi@mohos.name>
2018-09-12 13:45:46 +0200

    refactor(Test): CharacterizationTest invoke Class directly instead of shell_exec

--- test/Characterization/CharacterizationTest.php
+++ test/Characterization/CharacterizationTest.php
@@ -2,54 +2,39 @@
 
 namespace Test\Characterization;
 
+use App\Game\Game;
+use App\Game\GameRunner;
+use App\Logger\BufferedLogger;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * @runTestsInSeparateProcesses
+ */
 class CharacterizationTest extends TestCase
 {
+
     /**
      * @test
      */
-    public function v1_OriginalAndActualFilesAreEquals()
+    public function v1_OriginalCaptureFileAndGameOutputAreEquals()
     {
-        $this->assertVersion(1);
+        $this->assertCaptureEquals(Game::VERSION_DEFAULT);
     }
 
     /**
      * @test
      */
-    public function v2_OriginalAndActualFilesAreEquals()
+    public function v2_OriginalCaptureFileAndGameOutputAreEquals()
     {
-        $this->assertVersion(2);
+        $this->assertCaptureEquals(Game::VERSION_IMPROVED);
     }
 
     /**
      * @test
      */
-    public function v3_OriginalAndActualFilesAreEquals()
+    public function v3_OriginalCaptureFileAndGameOutputAreEquals()
     {
-        $this->assertVersion(3);
-    }
-
-    /**
-     * @param string $directory
-     * @param int $version
-     * @param int $seed
-     * @param string|null $scriptFile
-     */
-    private function generateTestFiles($directory, $version, $seed, $scriptFile = null)
-    {
-        if (null === $scriptFile) {
-            $scriptFile = __DIR__ . '/test_capture.php';
-        }
-        $outputFile = $this->getFilePath($directory, $version, $seed);
-        $dir = dirname($outputFile);
-        if(!file_exists($dir)) {
-            mkdir($dir, 0777, true);
-        }
-        if ($directory === 'original' && file_exists($outputFile)) {
-            return;
-        }
-        shell_exec(sprintf('php %s %d %d > %s', $scriptFile, $seed, $version, $outputFile));
+        $this->assertCaptureEquals(Game::VERSION_KICK_AND_ADD);
     }
 
     /**
@@ -60,21 +45,41 @@ class CharacterizationTest extends TestCase
      */
     private function getFilePath($directory, $version, $seed)
     {
-        return __DIR__ . '/' . $directory . '/v' . $version . '/game_' . $seed . '.txt';
+        return $directory . '/v' . $version . '/game_' . $seed . '.txt';
+    }
+
+    private function assertCaptureEquals(int $version, $assetsDir = __DIR__ . '/assets')
+    {
+        foreach (range(1, 10) as $savedGame) {
+            $output = $this->generateOutput($savedGame, $version);
+            $captureFile = $this->getFilePath($assetsDir, $version, $savedGame);
+            $this->generateCaptureFile($captureFile, $version, $savedGame);
+            $this->assertEquals(file_get_contents($captureFile), $output);
+        }
+    }
+
+    private function generateOutput($savedGame, $version)
+    {
+        $logger = new BufferedLogger();
+        (new GameRunner())->runSavedGame($savedGame, $version, $logger);
+        return $logger->getBuffer();
     }
 
     /**
-     * @param $version
+     * @param string $captureFile
+     * @param int $version
+     * @param int $savedGame
      */
-    private function assertVersion($version): void
+    private function generateCaptureFile($captureFile, $version, $savedGame)
     {
-        foreach (range(1, 10) as $seed) {
-            $this->generateTestFiles('original', $version, $seed);
-            $this->generateTestFiles('actual', $version, $seed);
-            $this->assertFileEquals(
-                $this->getFilePath('original', $version, $seed),
-                $this->getFilePath('actual', $version, $seed)
-            );
+        if (file_exists($captureFile)) {
+            return;
         }
+        $dir = dirname($captureFile);
+        if (!file_exists($dir)) {
+            mkdir($dir, 0777, true);
+        }
+        $output = $this->generateOutput($savedGame, $version);
+        file_put_contents($captureFile, $output);
     }
 }
