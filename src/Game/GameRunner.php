79bb60fb349bf04f0132ff754b7ed5cbdae1d8bb
Mohos Tamás <tomi@mohos.name>
2018-09-12 13:44:22 +0200

    feat(GameRunner): add runGame and runSavedGame method

--- src/Game/GameRunner.php
+++ src/Game/GameRunner.php
@@ -3,40 +3,84 @@
 namespace App\Game;
 
 use App\Categories;
+use App\Category;
+use App\Logger\LoggerInterface;
 use App\Player;
+use App\Question;
 
 class GameRunner
 {
-    private $notAWinner;
+    /**
+     * @int
+     */
+    const VERSION_DEFAULT = 1;
 
-    public function run(Categories $categories, array $players, int $version = 1)
+    /**
+     * @var bool
+     */
+    private $notAWinner = false;
+
+    public function runGame(Game $game, array $players)
     {
-        $aGame = new Game($categories, $version);
-
-        foreach($players as $player) {
-            $aGame->add(new Player($player));
+        foreach ($players as $player) {
+            $game->add(new Player($player));
         }
 
-        $iteration = 0;
+        $randomPlayerNumber = 1;
         do {
-            $aGame->roll(rand(0, 5) + 1);
+            $game->roll(rand(0, 5) + 1);
 
             if (rand(0, 9) === 7) {
-                $this->notAWinner = $aGame->wrongAnswer();
+                $this->notAWinner = $game->wrongAnswer();
             } else {
-                $playersCount = $aGame->howManyPlayers();
-                $this->notAWinner = $aGame->wasCorrectlyAnswered();
-                $newPlayersCount = $aGame->howManyPlayers();
-                if($version === Game::VERSION_KICK_AND_ADD && $playersCount !== $newPlayersCount) {
-                    $aGame->add(new Player('Random Player ' . $iteration));
-                }
-            }
-            if($version === Game::VERSION_KICK_AND_ADD) {
-                $iteration++;
-                if ($iteration === 300) {
-                    $this->notAWinner = false;
+                $playersCount = $game->howManyPlayers();
+                $this->notAWinner = $game->wasCorrectlyAnswered();
+                $newPlayersCount = $game->howManyPlayers();
+                if ($game->getVersion() === Game::VERSION_KICK_AND_ADD && $playersCount !== $newPlayersCount) {
+                    $game->add(new Player('Random Player ' . $randomPlayerNumber));
+                    $randomPlayerNumber++;
+                    if ($randomPlayerNumber > 10) {
+                        $this->notAWinner = false;
+                    }
                 }
             }
         } while ($this->notAWinner);
+    }
+
+    public function run(int $version = self::VERSION_DEFAULT, LoggerInterface $logger = null)
+    {
+        $this->runGame(new Game($this->getDefaultCategories(), $version, $logger), $this->getDefaultPlayers());
+    }
+
+    public function runSavedGame(int $savedGame, int $version = self::VERSION_DEFAULT, LoggerInterface $logger = null)
+    {
+        mt_srand($savedGame);
+        $this->run($version, $logger);
+    }
+
+    private function getDefaultCategories(): Categories
+    {
+        $questionsCount = 50;
+        $categories = new Categories();
+        $categories->addCategory(new Category('Pop', [0, 4, 8], 1));
+        $categories->addCategory(new Category('Science', [1, 5, 9], 1));
+        $categories->addCategory(new Category('Sports', [2, 6, 10], 1));
+        $categories->addCategory(new Category('Rock', [3, 7, 11], 1));
+
+        foreach (['Pop', 'Science', 'Sports', 'Rock'] as $category) {
+            foreach (range(0, $questionsCount) as $i) {
+                $categories->addQuestion($category, new Question($category . ' Question ' . $i));
+            }
+        }
+        return $categories;
+    }
+
+    private function getDefaultPlayers()
+    {
+        return [
+            'Chet',
+            'Pat',
+            'Sue'
+        ];
     }
 }
